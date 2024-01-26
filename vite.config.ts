9b8b8ba1030069afd218b589101ed2db6ae4c263
s4chin-verma <shivamvarma346@gmail.com>
2024-01-26 18:14:31 +0530

    Docker Setup

--- vite.config.ts
+++ vite.config.ts
@@ -9,4 +9,12 @@ export default defineConfig({
          '@': path.resolve(__dirname, './src'),
       },
    },
+   server: {
+      host: true,
+      port: 8080,
+      strictPort: true,
+      watch: {
+         usePolling: true,
+      },
+   },
 });
